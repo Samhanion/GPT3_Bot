0bc373bf86c220ce9d509841cef1433a57ad95c4
Samhan <samhanion98@gmail.com>
2022-09-20 01:47:15 +0200

    added an automatic response upon receiving a message.

--- routes/handleResponse.js
+++ routes/handleResponse.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET users listing. */
-router.post('/handle', function (req, res, next) {
+router.post('/handle', async function (req, res, next) {
   console.log(req.body);
+
+  if (req.body.status == 'RECEIVED') {
+    axios
+      .post(
+        'https://api.sendblue.co/api/send-message',
+        {
+          number: '+639956172447',
+          content: "The webhook received your message. Please don't reply back to this message.",
+          send_style: 'fireworks',
+          media_url: 'https://source.unsplash.com/random.png',
+          statusCallback: 'http://imessagebot-env-1.eba-452iz3ee.us-east-1.elasticbeanstalk.com/handle',
+        },
+        {
+          headers: {
+            'sb-api-key-id': '99b1ab1984188ec4bd9f107d8e786b87',
+            'sb-api-secret-key': '4c728b16d63f24ded5342744d63b06c2',
+            'content-type': 'application/json',
+          },
+        },
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   res.send('done!');
 });
 
